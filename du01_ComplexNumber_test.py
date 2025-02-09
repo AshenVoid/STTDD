5c1c29de1b25d25235d03a23c6d545176d22222b
AshenVoid <richardegydy@gmail.com>
2025-02-09 16:06:59 +0100

    DU1 + basic calc

--- du01_ComplexNumber_test.py
+++ du01_ComplexNumber_test.py
@@ -2,24 +2,55 @@ import pytest
 from du01_ComplexNumber import ComplexNumber
 
 def test_add():
+    # Sčítání kladných
     c1 = ComplexNumber(1,2)
     c2 = ComplexNumber(3,4)
-    assert c1.add(c2) == ComplexNumber(4,6)
+    assert c1.add(c2) == ComplexNumber(4,6)     #Exp. 4+4i
+    # Sčítání záporných
+    c3 = ComplexNumber(-1, -2)
+    c4 = ComplexNumber(3, 4)
+    assert c3.add(c4) == ComplexNumber(2, 2)    #Exp. 2+2i
+    #Sčítání s nulou
+    c5 = ComplexNumber(0, 0)
+    assert c1.add(c5) == c1     #Exp. 0+0i
 
 def test_subtract():
+    #Odčítání kladných
     c1 = ComplexNumber(5,6)
     c2 = ComplexNumber(2,3)
-    assert c1.subtract(c2) == ComplexNumber(3,3)
+    assert c1.subtract(c2) == ComplexNumber(3,3)    #Exp. 3+3i
+    # Odčítání záporných
+    c3 = ComplexNumber(-1, -2)
+    c4 = ComplexNumber(3, 4)
+    assert c3.subtract(c4) == ComplexNumber(-4, -6)     #Exp. -4-6i
+    #Odčítání s nulou
+    c5 = ComplexNumber(0, 0)
+    assert c1.subtract(c5) == c1    #Exp. 0-0i
 
 def test_multiply():
+    # Násobení kladných
     c1 = ComplexNumber(5, 6)
     c2 = ComplexNumber(2, 3)
-    assert c1.multiply(c2) == ComplexNumber(10, 18)
+    assert c1.multiply(c2) == ComplexNumber(10, 18)     #Exp. 10+18i
+    # Násobení záporných
+    c3 = ComplexNumber(-1, -2)
+    c4 = ComplexNumber(3, 4)
+    assert c3.multiply(c4) == ComplexNumber(-3, -8)     #Exp. -3-8i
+    # Nulou
+    c5 = ComplexNumber(0, 0)
+    assert c1.multiply(c5) == ComplexNumber(0, 0)   #Exp. 0+0i
 
 def test_divide():
+    #Dělení kladným
     c1 = ComplexNumber(4, 2)
     c2 = ComplexNumber(2, -2)
-    assert c1.divide(c2) == ComplexNumber(0.5, 1.5)
+    assert c1.divide(c2) == ComplexNumber(0.5, 1.5)     #Exp. 0.5+1.5i
+    #Dělení negativním číslem
+    c3 = ComplexNumber(-4, -2)
+    c4 = ComplexNumber(2, -2)
+    assert c3.divide(c4) == ComplexNumber(-0.5, -1.5)       #Exp. -0.5-1.5i
+    # Dělení sebou
+    assert c1.divide(c1) == ComplexNumber(1, 0)     #Exp. 1+0i
 
 def test_divide_by_zero():
     c1 = ComplexNumber(1, 1)
@@ -30,6 +61,12 @@ def test_divide_by_zero():
 def test_conjugate():
     c = ComplexNumber(3,4)
     assert c.conjugate() == ComplexNumber(3, -4)
+    # Test konjugátu 1
+    c2 = ComplexNumber(3, -4)
+    assert c2.conjugate() == ComplexNumber(3,4)
+    # Test konjugátu 2
+    c3 = ComplexNumber(5,0)
+    assert c3.conjugate()==ComplexNumber(5,0)
 
 def test_absolute_value():
     c = ComplexNumber(3,4)
--- p01_BasicCalc_test.py
+++ p01_BasicCalc_test.py
@@ -1,65 +1,67 @@
 import pytest
 from p01_BasicCalc import BasicCalc
 
-class Test:
-    def test_add(self):
-        basic_calc = BasicCalc()
-        result = basic_calc.add(3, 5)
-        assert result == 8
-        assert basic_calc.add(-6, -4) == -10
-        assert basic_calc.add(-3, 4) == 1
-        assert basic_calc.add(5, -6) == -1
-        assert basic_calc.add(5, 0) == 5
-        assert round(basic_calc.add(2.1,3.2), 5) == 5.3
-        assert round(basic_calc.add(10, 5.3), 5) == 15.3
-        assert basic_calc.add("3", "5") is None
-        assert basic_calc.add("Hello", "World") is None
-        assert basic_calc.add(3, "5") is None
-        assert basic_calc.add([3], 5) is None
+def test_add():
+    basic_calc = BasicCalc()
+    result = basic_calc.add(3, 5)
+    assert result == 8
+    assert basic_calc.add(-6, -4) == -10
+    assert basic_calc.add(-3, 4) == 1
+    assert basic_calc.add(5, -6) == -1
+    assert basic_calc.add(5, 0) == 5
+    assert round(basic_calc.add(2.1, 3.2), 5) == 5.3
+    assert round(basic_calc.add(10, 5.3), 5) == 15.3
+    assert basic_calc.add("3", "5") is None
+    assert basic_calc.add("Hello", "World") is None
+    assert basic_calc.add(3, "5") is None
+    assert basic_calc.add([3], 5) is None
 
-    def test_subtraction(self):
-        basic_calc = BasicCalc()
-        result = basic_calc.subtract(3, 5)
-        assert result == -2
-        assert basic_calc.subtract(-6, -4) == -2
-        assert basic_calc.subtract(-3, 4) == -7
-        assert basic_calc.subtract(5, -6) == 11
-        assert basic_calc.subtract(5, 0) == 5
-        assert round(basic_calc.subtract(2.1,3.2), 5) == -1.1
-        assert round(basic_calc.subtract(10, 5.3), 5) == 4.7
-        assert basic_calc.subtract("3", "5") is None
-        assert basic_calc.subtract("Hello", "World") is None
-        assert basic_calc.subtract(3, "5") is None
-        assert basic_calc.subtract([3], 5) is None
 
-    def test_multiply(self):
-        basic_calc = BasicCalc()
-        result = basic_calc.multiply(6, 3)
-        assert result == 18
-        assert basic_calc.multiply(-6, -2) == 12
-        assert basic_calc.multiply(-3, 3) == -9
-        assert basic_calc.multiply(6, -6) == -36
-        assert basic_calc.multiply(5, 0) == 0
-        assert round(basic_calc.multiply(2.1, 3.2), 5) == 6.72
-        assert round(basic_calc.multiply(11.25, 2.25), 5) == 25.3125
-        assert basic_calc.multiply("3", "5") is None
-        assert basic_calc.multiply("Hello", "World") is None
-        assert basic_calc.multiply(3, "5") is None
-        assert basic_calc.multiply([3], 5) is None
+def test_subtraction():
+    basic_calc = BasicCalc()
+    result = basic_calc.subtract(3, 5)
+    assert result == -2
+    assert basic_calc.subtract(-6, -4) == -2
+    assert basic_calc.subtract(-3, 4) == -7
+    assert basic_calc.subtract(5, -6) == 11
+    assert basic_calc.subtract(5, 0) == 5
+    assert round(basic_calc.subtract(2.1, 3.2), 5) == -1.1
+    assert round(basic_calc.subtract(10, 5.3), 5) == 4.7
+    assert basic_calc.subtract("3", "5") is None
+    assert basic_calc.subtract("Hello", "World") is None
+    assert basic_calc.subtract(3, "5") is None
+    assert basic_calc.subtract([3], 5) is None
 
-    def test_divide(self):
-        basic_calc = BasicCalc()
-        result = basic_calc.divide(6, 3)
-        assert result == 2
-        assert basic_calc.divide(-6, -2) == 3
-        assert basic_calc.divide(-3, 3) == -1
-        assert basic_calc.divide(6, -6) == -1
-        assert round(basic_calc.divide(2.1,3.2), 5) == 0.65625
-        assert round(basic_calc.divide(11.25, 2.25), 5) == 5
-        assert basic_calc.divide("3", "5") is None
-        assert basic_calc.divide("Hello", "World") is None
-        assert basic_calc.divide(3, "5") is None
-        assert basic_calc.divide([3], 5) is None
 
-        with pytest.raises(ZeroDivisionError):
-            assert basic_calc.divide(5,0)
+def test_multiply():
+    basic_calc = BasicCalc()
+    result = basic_calc.multiply(6, 3)
+    assert result == 18
+    assert basic_calc.multiply(-6, -2) == 12
+    assert basic_calc.multiply(-3, 3) == -9
+    assert basic_calc.multiply(6, -6) == -36
+    assert basic_calc.multiply(5, 0) == 0
+    assert round(basic_calc.multiply(2.1, 3.2), 5) == 6.72
+    assert round(basic_calc.multiply(11.25, 2.25), 5) == 25.3125
+    assert basic_calc.multiply("3", "5") is None
+    assert basic_calc.multiply("Hello", "World") is None
+    assert basic_calc.multiply(3, "5") is None
+    assert basic_calc.multiply([3], 5) is None
+
+
+def test_divide():
+    basic_calc = BasicCalc()
+    result = basic_calc.divide(6, 3)
+    assert result == 2
+    assert basic_calc.divide(-6, -2) == 3
+    assert basic_calc.divide(-3, 3) == -1
+    assert basic_calc.divide(6, -6) == -1
+    assert round(basic_calc.divide(2.1, 3.2), 5) == 0.65625
+    assert round(basic_calc.divide(11.25, 2.25), 5) == 5
+    assert basic_calc.divide("3", "5") is None
+    assert basic_calc.divide("Hello", "World") is None
+    assert basic_calc.divide(3, "5") is None
+    assert basic_calc.divide([3], 5) is None
+
+    with pytest.raises(ZeroDivisionError):
+        assert basic_calc.divide(5, 0)
